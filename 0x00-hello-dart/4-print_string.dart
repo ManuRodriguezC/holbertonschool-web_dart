4dd6d7b77af11c08f37cb98715ef0bf3349735fb
ManuRodriguezC <estebanclimb@gmail.com>
2024-03-21 12:39:52 -0500

    Dart - Hello Dart

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 void main() {
   String str = "Holberton School";
-  for (var i = 0; i < 3; i++) {
-    stdout.write(str);
-  }
-  print("\n${str.substring(0, 9)}\n");
+  print(str + str + str);
+  print("${str.substring(0, 9)}\n");
 }
